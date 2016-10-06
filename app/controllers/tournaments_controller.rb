e6bbc45d366658765b3ed3982733c2750eeab15f
Rami Ashtar <rami.ashtar@mail.utoronto.ca>
2016-10-06 00:08:03 -0400

    [#16] RA: Added embedded map & signup/sponsor buttons to tournament template

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,5 +1,12 @@
 class TournamentsController < ApplicationController
   def show
     @tournament = Tournament.find(params[:id])
+    get_golf_course_address
+  end
+
+  private
+  def get_golf_course_address
+    @golf_course_address = GolfCourse.find(@tournament.golf_course_id)
+    @golf_course_address = @golf_course_address.addrStreetNum.to_s + ' ' + @golf_course_address.addrStreetName + ' ' + @golf_course_address.addrPostalCode
   end
 end
