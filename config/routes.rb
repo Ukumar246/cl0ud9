9e6a9050c5c57f84f64eca4a78256ce07f4860a4
Rami Ashtar <rami.ashtar@mail.utoronto.ca>
2016-10-05 22:32:26 -0400

    [#16] RA: Added initial microsite template, using tournament get endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  # RA: Added tournaments as a resource, this provides us with useful endpoints
+  # that we'll probably use in the project (run: rails routes)
+  resources :tournaments
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/tournaments_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class TournamentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get a successful response when showing a tournament with a valid id" do
+    get tournament_path(tournaments(:one).id)
+    assert_response :success
+  end
+
+end
