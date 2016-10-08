df82fc20b5359d7a5b5fc5b7d2115a9517258b86
romina72 <romina.abachi@mail.utoronto.ca>
2016-10-07 20:25:10 -0400

    RAbachi: added index function to list all tournaments with links to microsites, added more seeds for Tournaments with real addresses

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,12 +1,26 @@
 class TournamentsController < ApplicationController
-  def show
+ 
+	def index
+		@addresses = Array.new
+
+		@tournaments = Tournament.all
+
+
+		@count = Tournament.count
+		#@tournaments.each do |t|
+		#	@addresses.push(get_golf_course_address(t))
+		#end
+  end
+
+	def show
     @tournament = Tournament.find(params[:id])
-    get_golf_course_address
+    @golf_course_address = get_golf_course_address(@tournament)
   end
 
   private
-  def get_golf_course_address
-    @golf_course_address = GolfCourse.find(@tournament.golf_course_id)
-    @golf_course_address = @golf_course_address.addrStreetNum.to_s + ' ' + @golf_course_address.addrStreetName + ' ' + @golf_course_address.addrPostalCode
+  def get_golf_course_address (tournament)
+    golf_course_address = GolfCourse.find(tournament.golf_course_id)
+    golf_course_address = golf_course_address.addrStreetNum.to_s + ' ' + 	golf_course_address.addrStreetName + ' ' + golf_course_address.addrPostalCode
+		return golf_course_address
   end
 end
