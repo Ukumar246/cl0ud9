878c052fefe62d8fb831424b77e5bf577743121c
Karsh <theutkarsh246@gmail.com>
2016-10-23 12:11:15 -0400

    Dynamically load player based on passed url id
    Note: player/profiles?id=34 is the current format for passing input

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,12 +1,14 @@
 class PlayerController < ApplicationController
   def profiles
     @people = Person.order(:fName).first(10)
-    @global = @people[3]
 
-    @people.each do |aPerson|
-      puts aPerson.inspect
-      puts "\n"
-    end
+	@person_id = params[:id].to_i
+	@global = @people[@person_id]
+	puts "Acessing Page: #@person_id"
+    # @people.each do |aPerson|
+    #   puts aPerson.inspect
+    #   puts "\n"
+   	#end
 
   end
 end
