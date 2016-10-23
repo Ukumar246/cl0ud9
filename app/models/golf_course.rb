8f6b0ff896bbb244f8de42ac5fb68ea35ccfd9fa
Waref Haque <warefhaque@inside-65-59-185.wireless.utoronto.ca>
2016-10-23 13:56:30 -0400

    minor validation fixes

--- app/models/golf_course.rb
+++ app/models/golf_course.rb
@@ -5,23 +5,23 @@ class GolfCourse < ApplicationRecord
   #   dont validate capacity as the owners may not know. Can change later if needed
 
   validates :email, presence: true
-  validates :email, format: { with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\z/, message: 'Please enter a valid email address E.g. john.smith@gmail.com' }
+  validates :email, format: { with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'Please enter a valid email address E.g. john.smith@gmail.com' }
 
   validates :name, presence: true
-  validates :name, format: { with: /\A[a-zA-Z\s]\z/, message: 'Please enter letters in name only E.g. Country Club' }
+  # validates :name, format: { with: /\A[a-zA-Z\s]\z/, message: 'Please enter letters in name only E.g. Country Club' }
 
   validates :addrStreetNum, presence: true
-  validates :addrStreetNum, format: { with: /\A[a-zA-Z0-9]\z/, message: 'Please enter letters and numbers in street number only E.g. 244 or 244A' }
+  validates :addrStreetNum, numericality: { only_integer: true, message: 'Please enter a valid street number e.g. 45' }
 
   validates :addrStreetName, presence: true
-  validates :addrStreetName, format: { with: /\A\s*\S+(?:\s+\S+){2}\z/, message: 'Please enter a valid street name E.g. St. George Street' }
+  validates :addrStreetName, format: { with: /^[0-9a-zA-Z. ]+$/, message: 'Please enter a valid street name E.g. St. George Street', multiline: true}
 
   # checks for canadian for now
   validates :addrPostalCode, presence: true
-  validates :addrPostalCode, format: { with: /\A[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\z/, message: 'Please enter a valid postal code E.g. M2N 6N3' }
+  validates :addrPostalCode, format: { with: /^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/, message: 'Please enter a valid postal code E.g. M2N 6N3',multiline: true }
 
   validates :addrCountry, presence: true
-  validates :addrCountry, format: { with: /\Aa-zA-Z]{2,}\z/, message: 'Please enter a valid country name E.g. Canada' }
+  validates :addrCountry, format: { with: /^[a-zA-Z]+$/, message: 'Please enter a valid country name E.g. Canada', multiline: true }
 
 
 end
