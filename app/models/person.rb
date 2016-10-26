5b67837ee2541bcba341601324a0b37d9b8f384f
Lawrence Temple <lawrence.temple@mail.utoronto.ca>
2016-10-25 23:42:35 -0400

    [15] Changed some of the views for the login, removed some old code pre devise.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,8 +2,10 @@ class Person < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable
+         :recoverable, :rememberable, :trackable, :validatable
+         
+         # Might need these later
+         # ,:confirmable, :lockable
 
   #Rabachi: for TOS agreement checking
   validates :terms_of_service, acceptance: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'golf_courses/new'
 
   devise_for :people
+
   get '/player/show/:id' => 'player#show'#, constraints: { id: /^[1-9][0-9]*$/ }
   #get 'player/show'
   get 'player/list'
-  get 'sessions/new'
   get 'welcome/index'
 
   #Rabachi: for the static pages
@@ -17,16 +17,6 @@ Rails.application.routes.draw do
 		resources :photos
 		resources :sponsorships
   end
-
-  # LT: Added some aliases(?) for people actions and some for sessions (not yet implemented)
-  # TODO: Must update autotesters to test these endpoints
-  get    '/signup',  to: 'people#new',  :as => :person
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-
-  # LT: Added people as a resource. Not sure how many of these we'll use, but yea.
-  resources :people
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
