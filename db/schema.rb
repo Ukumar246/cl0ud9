bd2fea58b3e47a454610c6c31d98aaed08274bef
Lawrence Temple <lawrence.temple@mail.utoronto.ca>
2016-11-05 02:50:55 -0400

    [63] Only found one other keyword, removed it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105054720) do
+ActiveRecord::Schema.define(version: 20161105064519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,7 +129,7 @@ ActiveRecord::Schema.define(version: 20161105054720) do
 
   create_table "scheduled_events", force: :cascade do |t|
     t.integer  "tournament_id"
-    t.time     "time"
+    t.time     "startTime"
     t.text     "description"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
