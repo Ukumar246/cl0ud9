88a6a8b95ecc03a4d5f94e66ae9cca67d5e53962
Grace Cochrane <grace.cochrane@mail.utoronto.ca>
2016-11-06 21:30:31 +0000

    [48] Added basic new player form - must be signed in. Fixed all folders/files that were player instead of players

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,3 +1,7 @@
 class TicketType < ApplicationRecord
   belongs_to :tournament
+
+  def select_option
+  	"#{name} $#{'%.2f' %price}"
+  end
 end
--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PlayerControllerTest < ActionDispatch::IntegrationTest
+class PlayersControllerTest < ActionDispatch::IntegrationTest
   # test "should get profiles" do
   #   get player_profiles_url
   #   assert_response :success
