6de351493c9977af3c6ed5b593ac87aef140e562
Grace Cochrane <grace.cochrane@mail.utoronto.ca>
2016-11-12 17:08:02 +0000

    Added uniqueness check on qrcodes, and changed tournament info to display num guests attending so far, not total

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,12 @@ class Player < ApplicationRecord
   belongs_to :ticket_type
 
   attr_accessor :numTickets
+
+  validates :QRCodeStr, uniqueness: true, if: ':QRCodeStr.present?'
+
+  after_create(on: :create) do |p|
+  	#so that qr codes will be unique even if one person buys a number of tickets
+  	p.QRCodeStr = p.person.email + p.id.to_s + p.ticket_type.name + p.tournament.name + DateTime.now.to_s
+  	p.save
+  end
 end
