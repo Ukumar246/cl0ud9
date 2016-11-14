e803c28a2fa6f1a8a5d7d21501c577fa70149a03
warefhaque <warefhaque@Warefs-MacBook-Pro.local>
2016-11-13 21:12:23 -0500

    players controller--- querying the required information for each of the types of people (i.e players, sponsors etc).

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -16,7 +16,53 @@ class PlayersController < ApplicationController
     end
 
     @golf_course_people = GolfCoursePerson.where(person_id:@player_id)
-    
+    if @golf_course_people.size>0
+
+      golf_courses = []
+      print('@golf_course_people EXISTS!');
+    #   need two tabs for this: golf courses && hosted tournaments
+      @golf_course_people.each do |golf_course_person|
+          golf_courses << golf_course_person.golf_course_id
+      end
+
+      @golf_courses = GolfCourse.where(id:golf_courses)
+      @tournaments_golf_courses = Tournament.where(golf_course_id:golf_courses)
+
+    end
+    @organizers = Organizer.where(person_id:@player_id)
+    if @organizers.size > 0
+
+      tournaments_organizers = []
+      print('@organizers EXISTS!');
+      @organizers.each do |organizer|
+        tournaments_organizers << organizer.tournament_id
+      end
+
+      @tournaments_organizers = Tournament.where(id:tournaments)
+
+    end
+    @players = Player.where(person_id:@player_id)
+    if  @players.size > 0
+
+      tournaments_players = []
+      print('@players  EXISTS!');
+      @players.each do |player|
+        tournaments_players << player.tournament_id
+      end
+
+      @tournaments_players = Tournament.where(id:tournaments_players)
+
+    end
+    @sponsors = Sponsor.where(person_id:@player_id)
+    if @sponsors.size > 0
+
+      tournaments_sponsors = []
+      @sponsors.each do |sponsor|
+        tournaments_sponsors << sponsor.tournament_id
+      end
+
+      @tournaments_sponsors = Tournament.where(id:tournaments_sponsors)
+    end
   end
 
   def new
