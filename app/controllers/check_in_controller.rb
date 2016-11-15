4e5ba757f4e1f6c9e54776057e545d449d20cea3
abhishek10452 <abhishek10452@gmail.com>
2016-11-14 19:09:59 -0500

    checkin
    check in

--- app/controllers/check_in_controller.rb
+++ app/controllers/check_in_controller.rb
@@ -1,4 +1,16 @@
 class CheckInController < ApplicationController
+
+  #show all the player's on our database
+  def list
+    @tplayers = Player.all
+  end
+
+  def show
+    @tourPlayer = Player.where(tournament_id:1)
+  end
+
+
+
   def show
   end
 end
