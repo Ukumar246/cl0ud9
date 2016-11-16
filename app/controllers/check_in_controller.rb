83cfb1a578d9fb4f2b5abbe30fa0e8c16ffeedc0
Karsh <theutkarsh246@gmail.com>
2016-11-16 15:54:08 -0500

    Check In Player's Front end Code

--- app/controllers/check_in_controller.rb
+++ app/controllers/check_in_controller.rb
@@ -1,16 +1,17 @@
 class CheckInController < ApplicationController
+  def index 
+  end
 
   #show all the player's on our database
   def list
     @tplayers = Player.all
+    @pass_str = 'Pass this str'
   end
 
   def show
+  	puts "Check In Controller..."
     @tourPlayer = Player.where(tournament_id:1)
-  end
-
-
-
-  def show
+    @avatar_link = "http://www.w3schools.com/w3css/img_avatar2.png"
+    @players = Array[1, 2, 3, 4,5];
   end
 end
