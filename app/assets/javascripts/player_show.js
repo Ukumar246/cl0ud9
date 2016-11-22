afca63a765af4ea736a40883035a2c5cebd0079e
warefhaque <warefhaque@inside-65-56-13.wireless.utoronto.ca>
2016-11-21 20:38:25 -0500

    jquery + ruby code to switch the tabs in the profile correctly and display only the information that is required per person.

--- app/assets/javascripts/player_show.js
+++ app/assets/javascripts/player_show.js
@@ -2,24 +2,33 @@
  * Created by warefhaque on 11/14/16.
  */
  $(document).ready(function () {
-
+     $('#no-data').hide();
+     var firstTab = $('.nav-item a').first();
+     firstTab.addClass('active');
+     var id = firstTab.attr('id');
+     console.log('id: '+id);
+     if (id==undefined){
+        $('.card').hide();
+        $('#no-data').show();
+     }
+     switchTabs(id);
 
      $(".nav-item a").on("click",function () {
-
-     $('.card-block').hide();
-     var nav_id  = $(this).attr('id');
-
-     console.log("CLICKED THE NAV ITEM");
-     $(".nav-item a").each(function (i,e) {
-         $(e).removeClass('active');
-     });
-     $(this).addClass('active');
-
-     var split_id = nav_id.split("-");
-     var card_id = 'card-'+split_id[1];
-
-     $('#'+card_id).show();
-     console.log(card_id);
-     return false;
+         switchTabs($(this).attr('id'));
+         return false;
    });
+
+     function switchTabs(nav_id){
+         $('.card-block').hide()
+         $(".nav-item a").each(function (i,e) {
+             $(e).removeClass('active');
+         });
+         var tab = $("#"+nav_id);
+         tab.addClass('active');
+
+         var split_id = nav_id.split("-");
+         var card_id = 'card-'+split_id[1];
+
+         $('#'+card_id).show();
+     }
  });
