64aa6bcb2eae90b120110d395153382c0114abc6
Lawrence Temple <lawrence.temple@mail.utoronto.ca>
2016-12-03 03:58:36 -0500

    [99] Last touch ups on golf course indexies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Route exceptions to the application router vs. default
-  config.exceptions_app = self.routes
 end
