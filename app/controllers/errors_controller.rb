013ee308822560fd2d7694df361183cf6ac5a54f
Lawrence Temple <lawrence.temple@mail.utoronto.ca>
2016-12-03 04:12:24 -0500

    [Maintainance] Trying to get the 404 page working again

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@ class ErrorsController < ApplicationController
 
   def show
     status_code = params[:code] || 500
-    flash[:danger] = "Status #{status_code}"
+    flash.alert = "Status #{status_code}"
     render "404", status: status_code
   end
 
