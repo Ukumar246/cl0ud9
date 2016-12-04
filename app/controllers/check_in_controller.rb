6378a61eaeafbfc64e99de88524661652ddeaf2d
Karsh <theutkarsh246@gmail.com>
2016-12-04 16:04:12 -0500

    Check In for multiple players form submission

--- app/controllers/check_in_controller.rb
+++ app/controllers/check_in_controller.rb
@@ -12,6 +12,7 @@ class CheckInController < ApplicationController
   	puts "Check In Controller..."
     
     @tourPlayers = Player.where(tournament_id:params[:id])
+    puts "Players in this tournament: #{@tourPlayers.length}"
     #an_id = @tourPlayers[0].person_id;
     #@specificPlayer = Person.where(person_id:an_id)
     #puts "Players who can sign up: #{@tourPlayers.length} & #{@tourPlayers[0].person.profilePicLink}"
@@ -20,16 +21,17 @@ class CheckInController < ApplicationController
 
 
   def submit
-    player_id = params[:player_id];
-    puts "you submitted this form #{player_id}"
-    @aplayer = Player.find(player_id)
-    puts "player -> #{@aplayer.person.email}"
-    @aplayer.checkedIn = true;
+    params[:player_ids].each do |player_id|
+      puts "you submitted this form for player id: #{player_id}"
+      @aplayer = Player.find(player_id)
+      puts "player -> #{@aplayer.person.email}"
+      @aplayer.checkedIn = true;
 
-    if @aplayer.save 
-      puts "Saved!"
-    else
-      puts "Error: cant save this player"
+      if @aplayer.save 
+        puts "Saved!"
+      else
+        puts "Error: cant save this player"
+      end
     end
   end
 end
