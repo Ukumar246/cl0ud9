87c2869d9e31dec52f4e77b4e36fe315676bd54d
Rami Ashtar <rami.ashtar@mail.utoronto.ca>
2016-12-05 22:27:12 -0500

    RA: Fixed issue with cloudinary pics not uploading by adding mount

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,12 +3,13 @@ class Person < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
          # Might need these later
          # ,:confirmable, :lockable
 
   has_many :players
   has_many :organizers
+  mount_uploader :profilePicLink, PhotoUploader
 
   #Rabachi: for TOS agreement checking
   validates :terms_of_service, acceptance: true
